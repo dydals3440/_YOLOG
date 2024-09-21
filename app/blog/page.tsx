4921eb35325b47bc37077ce2fefb390a1dcbea68
김용민 <dydals3440@gmail.com>
2024-09-22 01:07:03 +0900

    feat: Navbar Loading

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -1,21 +1,35 @@
 import Link from 'next/link'
 import { buttonVariants } from '@/components/ui/button'
 import SampleBlogs from '@/config/sample-blogs'
+import fs, { readFileSync } from 'fs'
+import matter from 'gray-matter'
 
 interface IBlog {
   slug: string
-  content: string
   title: string
   description: string
-  imageUrl: string
+  imageUrl?: string
 }
 
-const BlogPage = ({ blogs }: { blogs: IBlog[] }) => {
+const dirContent = fs.readdirSync('content', 'utf-8')
+const blogs: IBlog[] = dirContent.map((file) => {
+  const fileContent = readFileSync(`content/${file}`, 'utf-8')
+  const { data } = matter(fileContent)
+  const value: IBlog = {
+    slug: data.slug,
+    title: data.title,
+    description: data.description,
+    imageUrl: data?.imageUrl,
+  }
+  return value
+})
+
+const BlogPage = () => {
   return (
     <div className="container mx-auto p-4">
       <h1 className="text-3xl font-bold mb-6 text-center my-2">Our Blogs</h1>
       <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6">
-        {SampleBlogs.map((blog, index: number) => (
+        {blogs.map((blog: IBlog, index: number) => (
           <div key={index} className="shadow-lg rounded-lg overflow-hidden">
             <img
               className="w-full h-64 object-cover object-top"
@@ -38,5 +52,4 @@ const BlogPage = ({ blogs }: { blogs: IBlog[] }) => {
     </div>
   )
 }
-
 export default BlogPage
--- components/navbar/navbar.tsx
+++ components/navbar/navbar.tsx
@@ -1,3 +1,5 @@
+'use client'
+
 import Link from 'next/link'
 import { buttonVariants } from '@/components/ui/button'
 import { ModeToggle } from '@/components/navbar/theme-toggle'
@@ -10,57 +12,78 @@ import {
   SheetTitle,
   SheetTrigger,
 } from '@/components/ui/sheet'
+import LoadingBar from 'react-top-loading-bar'
+import { useEffect, useState } from 'react'
+import { usePathname } from 'next/navigation'
 
 const Navbar = () => {
+  const [progress, setProgress] = useState(0)
+  const pathname = usePathname()
+  useEffect(() => {
+    setProgress(30)
+    setTimeout(() => {
+      setProgress(100)
+    }, 100)
+  }, [pathname])
+
+  useEffect(() => {
+    setProgress(0)
+  }, [])
+
   return (
     <nav className="h-16 bg-background/50 sticky top-0 border-b px-8 backdrop-blur flex items-center justify-between z-10">
-      <div className="text-lg font-bold md:text-xl">YOLOG</div>
+      <LoadingBar
+        color="#000"
+        progress={progress}
+        onLoaderFinished={() => setProgress(0)}
+      />
+      <div className="text-lg font-bold md:text-xl">
+        <Link href={'/'}>YOLOG</Link>
+      </div>
       <ul className="hidden md:flex w-full justify-end items-center space-x-4 ">
         <li>
-          <Link href="/">Home</Link>
+          <Link href={'/'}>Home</Link>
         </li>
         <li>
-          <Link href="/about">About</Link>
+          <Link href={'/about'}>About</Link>
         </li>
         <li>
-          <Link href="/blog">Blog</Link>
+          <Link href={'/blog'}>Blog</Link>
         </li>
         <li>
-          <Link href="/contact">Contact</Link>
+          <Link href={'/contact'}>Contact</Link>
         </li>
-        <li className="flex px-4 items-center space-x-1">
+        <li className="buttons px-4 space-x-2">
           <Link
-            href="/login"
+            href={'/login'}
             className={buttonVariants({ variant: 'outline' })}
           >
             Login
           </Link>
           <Link
-            href="/signup"
+            href={'/login'}
             className={buttonVariants({ variant: 'outline' })}
           >
             Sign Up
           </Link>
         </li>
       </ul>
-      <div className="flex gap-x-4">
-        <ModeToggle />
-        <div className="flex items-center justify-center sm:hidden">
-          <Sheet>
-            <SheetTrigger className="md:hidden">
-              <HamburgerMenuIcon className="size-6" />
-            </SheetTrigger>
-            <SheetContent>
-              <SheetHeader>
-                <SheetTitle>Are you absolutely sure?</SheetTitle>
-                <SheetDescription>
-                  This action cannot be undone. This will permanently delete
-                  your account and remove your data from our servers.
-                </SheetDescription>
-              </SheetHeader>
-            </SheetContent>
-          </Sheet>
-        </div>
+      <ModeToggle />
+      <div className="flex items-center justify-center sm:hidden">
+        <Sheet>
+          <SheetTrigger className="md:hidden">
+            <HamburgerMenuIcon className="size-6" />
+          </SheetTrigger>
+          <SheetContent>
+            <SheetHeader>
+              <SheetTitle>Are you absolutely sure?</SheetTitle>
+              <SheetDescription>
+                This action cannot be undone. This will permanently delete your
+                account and remove your data from our servers.
+              </SheetDescription>
+            </SheetHeader>
+          </SheetContent>
+        </Sheet>
       </div>
     </nav>
   )
