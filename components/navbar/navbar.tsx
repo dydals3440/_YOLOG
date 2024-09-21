0c7dc79e90022feafad1fab3bf96c518016f5050
김용민 <dydals3440@gmail.com>
2024-09-21 22:42:39 +0900

    feat: Navbar Mobile 반응형 대응

--- components/navbar/navbar.tsx
+++ components/navbar/navbar.tsx
@@ -1,12 +1,21 @@
 import Link from 'next/link'
 import { buttonVariants } from '@/components/ui/button'
 import { ModeToggle } from '@/components/navbar/theme-toggle'
+import { HamburgerMenuIcon } from '@radix-ui/react-icons'
+import {
+  Sheet,
+  SheetContent,
+  SheetDescription,
+  SheetHeader,
+  SheetTitle,
+  SheetTrigger,
+} from '@/components/ui/sheet'
 
 const Navbar = () => {
   return (
-    <nav className="h-16 bg-background/60 sticky top-0 border-b backdrop-blur flex px-8 items-center">
-      <div className="font-bold text-xl">YOLOG</div>
-      <ul className="flex w-full justify-end items-center gap-x-4">
+    <nav className="h-16 bg-background/50 sticky top-0 border-b px-8 backdrop-blur flex items-center justify-between z-10">
+      <div className="text-lg font-bold md:text-xl">YOLOG</div>
+      <ul className="hidden md:flex w-full justify-end items-center space-x-4 ">
         <li>
           <Link href="/">Home</Link>
         </li>
@@ -27,10 +36,26 @@ const Navbar = () => {
             Sign Up
           </Link>
         </li>
-        <li>
-          <ModeToggle />
-        </li>
       </ul>
+      <div className="flex gap-x-4">
+        <ModeToggle />
+        <div className="flex items-center justify-center sm:hidden">
+          <Sheet>
+            <SheetTrigger className="md:hidden">
+              <HamburgerMenuIcon className="size-6" />
+            </SheetTrigger>
+            <SheetContent>
+              <SheetHeader>
+                <SheetTitle>Are you absolutely sure?</SheetTitle>
+                <SheetDescription>
+                  This action cannot be undone. This will permanently delete
+                  your account and remove your data from our servers.
+                </SheetDescription>
+              </SheetHeader>
+            </SheetContent>
+          </Sheet>
+        </div>
+      </div>
     </nav>
   )
 }
